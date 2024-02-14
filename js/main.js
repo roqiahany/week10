5dd671ef1f1690b1462f5c0df8e871515957cd4a
Roqia Hany <roqiahani5@gmail.com>
2024-02-14 08:01:06 +0200

    new Session (events and example with it 'slider')

--- js/main.js
+++ js/main.js
@@ -1,212 +1,99 @@
-var nameProductInput = document.getElementById('productName');
-var priceProductInput = document.getElementById('productPrice');
-var categoryProductInput = document.getElementById('productCategory');
-var descriptionProductInput = document.getElementById('productDescription');
-// console.log(nameProductInput);
-var searchInput = document.getElementById('searchInput');
+// var test = document.getElementById('test');
 
-var addBTN = document.getElementById('addBTN');
-var updateBTN = document.getElementById('updateBTN');
-var indexNum = 0;
+// test.addEventListener('contextmenu', function (e) {
+//   e.preventDefault();
+//   console.log('hello');
+// });
 
-var producrContainer = [];
-if (localStorage.getItem('products') != null) {
-  producrContainer = JSON.parse(localStorage.getItem('products'));
-  displayData();
+// var demo = document.getElementById('dataForm');
+// demo.addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   console.log('hi');
+// });
+
+// var img = document.getElementById('img');
+// img.addEventListener('drag', function () {
+//   console.log('koko');
+// });
+
+//!------------------- Global Var ---------------------
+// DOM --> element , event , action
+var imgs = Array.from(document.querySelectorAll('.item img')); // convert node List to array
+var lightBox = document.getElementById('lightBox');
+var closeIcon = document.getElementById('close');
+var inBox = document.getElementById('inBox');
+var currentIndex = 0;
+var nextImg = document.getElementById('next');
+var prevImg = document.getElementById('prev');
+
+//!------------------- functions ---------------------
+function closeSlide(from, to) {
+  lightBox.classList.replace(from, to);
 }
 
-function addProduct() {
-  if (
-    validationName() == true &&
-    validationPrice() == true &&
-    validationCateg() == true &&
-    validationDesc()
-  ) {
-    var product = {
-      name: nameProductInput.value,
-      price: priceProductInput.value,
-      cate: categoryProductInput.value,
-      desc: descriptionProductInput.value,
-    };
-    producrContainer.push(product);
-    console.log(producrContainer);
-    localStorage.setItem('products', JSON.stringify(producrContainer));
-    displayData();
-    clearForm();
-    removeIsValid();
+function nextSlide() {
+  currentIndex++; // next Index
+
+  if (currentIndex == imgs.length) {
+    currentIndex = 0;
   }
+
+  // img kolo --->src
+  var currentEleSrc = imgs[currentIndex].getAttribute('src');
+
+  inBox.style.backgroundImage = `url(${currentEleSrc})`;
 }
 
-function displayData() {
-  var cartona = '';
-  for (var i = 0; i < producrContainer.length; i++) {
-    cartona += `<tr>
-              <td>${producrContainer[i].name}</td>
-              <td>${producrContainer[i].price}</td>
-              <td>${producrContainer[i].cate}</td>
-              <td>${producrContainer[i].desc}</td>
-              <td>
-                <button class="btn btn-outline-warning btn-sm" onClick='setProduct(${i})'>update</button>
-                <button class="btn btn-outline-danger btn-sm "  onclick="deleteProduct(${i})">delete</button>
-                </td>
-            </tr>`;
+function prevSlide() {
+  currentIndex--; // prev Index
+
+  if (currentIndex < 0) {
+    currentIndex = imgs.length - 1;
   }
-  document.getElementById('tableData').innerHTML = cartona;
+
+  // img kolo --->src
+  var currentEleSrc = imgs[currentIndex].getAttribute('src');
+
+  inBox.style.backgroundImage = `url(${currentEleSrc})`;
 }
 
-function deleteProduct(elementNum) {
-  producrContainer.splice(elementNum, 1);
-  localStorage.setItem('products', JSON.stringify(producrContainer));
-  displayData();
+//!------------------- Events ---------------------
+for (var i = 0; i < imgs.length; i++) {
+  imgs[i].addEventListener('click', function (e) {
+    closeSlide('d-none', 'd-block');
+    var currentTarget = e.target; // img select
+    currentIndex = imgs.indexOf(currentTarget); // will search and go back with *index* if exist in array
+    var currentSrc = e.target.getAttribute('src');
+    console.log(e.target.getAttribute('src'));
+
+    inBox.style.backgroundImage = `url(${currentSrc})`;
+  });
 }
 
-function searchProduct() {
-  var term = searchInput.value;
-  var cartona = '';
-  for (var i = 0; i < producrContainer.length; i++) {
-    if (producrContainer[i].name.toLowerCase().includes(term.toLowerCase())) {
-      cartona += `<tr>
-              <td>${producrContainer[i].name}</td>
-              <td>${producrContainer[i].price}</td>
-              <td>${producrContainer[i].cate}</td>
-              <td>${producrContainer[i].desc}</td>
-              <td>
-                <button class="btn btn-outline-warning btn-sm" >update</button>
-                <button class="btn btn-outline-danger btn-sm "  onclick="deleteProduct(${i})">delete</button>
-                </td>
-            </tr>`;
-    }
-    document.getElementById('tableData').innerHTML = cartona;
+closeIcon.addEventListener('click', function () {
+  closeSlide('d-block', 'd-none');
+});
+
+nextImg.addEventListener('click', function () {
+  nextSlide();
+});
+
+prevImg.addEventListener('click', function () {
+  prevSlide();
+});
+
+document.addEventListener('click', function (e) {
+  if (e.target == lightBox) {
+    closeSlide('d-block', 'd-none');
   }
-}
+});
 
-function setProduct(index) {
-  indexNum = index;
-  var currentProduct = producrContainer[index];
-  nameProductInput.value = currentProduct.name;
-  priceProductInput.value = currentProduct.price;
-  categoryProductInput.value = currentProduct.cate;
-  descriptionProductInput.value = currentProduct.desc;
-
-  addBTN.classList.add('d-none');
-  updateBTN.classList.remove('d-none');
-}
-
-function updateProduct() {
-  var product = {
-    name: nameProductInput.value,
-    price: priceProductInput.value,
-    cate: categoryProductInput.value,
-    desc: descriptionProductInput.value,
-  };
-  producrContainer.splice(indexNum, 1, product);
-  console.log(producrContainer);
-  displayData();
-  localStorage.setItem('products', JSON.stringify(producrContainer));
-
-  addBTN.classList.remove('d-none');
-  updateBTN.classList.add('d-none');
-
-  clearForm();
-}
-
-function clearForm() {
-  nameProductInput.value = '';
-  priceProductInput.value = '';
-  categoryProductInput.value = '';
-  descriptionProductInput.value = '';
-}
-
-// var regexName = /^[A-Z][a-z]{2,8}$/;
-
-// var text = 'Roqia';
-// var text_2 = 'roqia';
-
-// console.log(regexName.test(text)); // true
-// console.log(regexName.test(text_2)); // false
-
-function validationName() {
-  var messageName = document.getElementById('messageName');
-  var regexName = /^[A-Z][a-z]{2,8}$/;
-  var text = nameProductInput.value;
-  console.log(regexName.test(text));
-  if (regexName.test(text)) {
-    // valid
-    nameProductInput.classList.add('is-valid');
-    nameProductInput.classList.remove('is-invalid');
-    messageName.classList.add('d-none');
-    return true;
-  } else {
-    // not Valid
-    nameProductInput.classList.remove('is-valid');
-    nameProductInput.classList.add('is-invalid');
-    messageName.classList.remove('d-none');
-    return false;
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'ArrowRight') {
+    nextSlide();
+  } else if (e.key == 'ArrowLeft') {
+    prevSlide();
+  } else if (e.key == 'Escape') {
+    closeSlide('d-block', 'd-none');
   }
-}
-function validationPrice() {
-  var messagePrice = document.getElementById('messagePrice');
-  var regexPrice = /^[1-9][0-9]$/;
-  var text = priceProductInput.value;
-  console.log(regexPrice.test(text));
-  if (regexPrice.test(text)) {
-    // valid
-    priceProductInput.classList.add('is-valid');
-    priceProductInput.classList.remove('is-invalid');
-    messagePrice.classList.add('d-none');
-    return true;
-  } else {
-    // not Valid
-    priceProductInput.classList.remove('is-valid');
-    priceProductInput.classList.add('is-invalid');
-    messagePrice.classList.remove('d-none');
-    return false;
-  }
-}
-function validationCateg() {
-  var messageCateg = document.getElementById('messageCateg');
-  //  category is Invalid You must enter just char and at least 3 char
-  var regexCateg = /^[A-Z]?[a-z]{2,100}$/;
-  var text = categoryProductInput.value;
-  console.log(regexCateg.test(text));
-  if (regexCateg.test(text)) {
-    // valid
-    categoryProductInput.classList.add('is-valid');
-    categoryProductInput.classList.remove('is-invalid');
-    messageCateg.classList.add('d-none');
-    return true;
-  } else {
-    // not Valid
-    categoryProductInput.classList.remove('is-valid');
-    categoryProductInput.classList.add('is-invalid');
-    messageCateg.classList.remove('d-none');
-    return false;
-  }
-}
-function validationDesc() {
-  var messageDesc = document.getElementById('messageDesc');
-  //  Descory is Invalid You must enter just char and at least 3 char
-  var regexDesc = /^[A-Z]?[a-z]{2,100}$/;
-  var text = descriptionProductInput.value;
-  console.log(regexDesc.test(text));
-  if (regexDesc.test(text)) {
-    // valid
-    descriptionProductInput.classList.add('is-valid');
-    descriptionProductInput.classList.remove('is-invalid');
-    messageDesc.classList.add('d-none');
-    return true;
-  } else {
-    // not Valid
-    descriptionProductInput.classList.remove('is-valid');
-    descriptionProductInput.classList.add('is-invalid');
-    messageDesc.classList.remove('d-none');
-    return false;
-  }
-}
-
-function removeIsValid() {
-  nameProductInput.classList.remove('is-valid');
-  priceProductInput.classList.remove('is-valid');
-  categoryProductInput.classList.remove('is-valid');
-  descriptionProductInput.classList.remove('is-valid');
-}
+});
